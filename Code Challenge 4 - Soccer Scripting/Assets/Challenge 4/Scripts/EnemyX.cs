9ac17e8669b69a076fb9ccecea3558a1c8fae85b
Th3Equin0x <sgruver22@gmail.com>
2019-10-01 23:33:57 -0500

    did the thing

---
+++ Code Challenge 4 - Soccer Scripting/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,48 @@
+﻿/*
+		 * Stephen Gruver
+		 * EnemyX.cs
+		 * Assignment 6
+		 * Controls the behavior of the enemy characters
+		 */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    public GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
